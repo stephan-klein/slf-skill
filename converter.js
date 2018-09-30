2a513ef1992956d8cc4facdd9c74618e9859597f
Stephan <sklein.at@gmail.com>
2018-09-30 17:47:57 +0200

    Add Cars and Animals

--- converter.js
+++ converter.js
@@ -2,15 +2,15 @@ module.exports = {
   convertCsvToJson: function(){
     console.log('Hello World');
 
-    const colorsobj = require('./csv/colors');
-    let colors = colorsobj.colors;
+    const dataobj = require('./csv/csvdata');
+    let data = dataobj.cars;
 
     const struct = {
 
     };
 
-    for (let i = 0; i < colors.length; i++){
-      let color = colors[i];
+    for (let i = 0; i < data.length; i++){
+      let color = data[i];
       color = color.charAt(0).toUpperCase() + color.substring(1).toLowerCase();
       let letter = color.charAt(0).toUpperCase();
 
@@ -27,6 +27,15 @@ module.exports = {
     });
 
     console.log(ordered);
+
+    let fs = require('fs');
+    fs.writeFile("data.out", JSON.stringify(ordered), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+
+      console.log("The file was saved!");
+    });
   }
 };
 require('make-runnable');
